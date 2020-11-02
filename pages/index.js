e3dcd0d1758e8e0707bf895e258f7e76cebfe9ed
Alejandro Foronda <aforonda90@gmail.com>
2020-11-02 02:06:12 +0000

    Change headline

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 export default function IndexPage() {
-  return (
-    <div>
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
-      </div>
-    </div>
-  )
+	return (
+		<div className="bg-gray-900 flex justify-center min-h-screen min-w-screen">
+			<div className="pt-40">
+				<h1 className="text-gray-100 font-bold text-center text-lg">
+					Making great web experiences for education and productivity
+				</h1>
+			</div>
+		</div>
+	);
 }
